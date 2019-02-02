d14a25c9b8b8040c54fbbfed743be415943b7465
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2019-02-02 21:01:47 +0200

    first commit

--- src/com/javaguru/todolist/ToDoListApplication.java
+++ src/com/javaguru/todolist/ToDoListApplication.java
@@ -1,12 +1,16 @@
 package com.javaguru.todolist;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ToDoListApplication {
 
+    private static Long TASK_ID_SEQUENCE = 0L;
+
     public static void main(String[] args) {
-        TaskService taskService = new TaskService();
         Scanner scanner = new Scanner(System.in);
+        Map<Long, Task> tasks = new HashMap<>();
 
         while (true) {
             try {
@@ -16,10 +20,10 @@ public class ToDoListApplication {
                 int userInput = scanner.nextInt();
                 switch (userInput) {
                     case 1:
-                        taskService.createTask();
+                        createTask(tasks);
                         break;
                     case 2:
-                        taskService.findTask();
+                        findTask(tasks);
                         break;
                     case 3:
                         return;
@@ -28,5 +32,32 @@ public class ToDoListApplication {
                 System.out.println("Error! Please try again.");
             }
         }
+    }
+
+    private static void createTask(Map<Long, Task> tasks) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter task name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter task description: ");
+        String description = scanner.nextLine();
+
+        Task task = new Task();
+        task.setName(name);
+        task.setDescription(description);
+
+        task.setId(TASK_ID_SEQUENCE);
+
+        tasks.put(TASK_ID_SEQUENCE, task);
+
+        System.out.println("Task created, id:  " + task.getId());
+        TASK_ID_SEQUENCE++;
+    }
+
+    private static void findTask(Map<Long, Task> tasks) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter task id: ");
+        Long id = scanner.nextLong();
+        Task task = tasks.get(id);
+        System.out.println(task);
     }
 }
