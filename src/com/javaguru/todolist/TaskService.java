5fe911b33d762ca12dcbd1814aebd1f03393c9d8
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2019-02-02 20:51:03 +0200

    first commit

---
+++ src/com/javaguru/todolist/TaskService.java
@@ -0,0 +1,38 @@
+package com.javaguru.todolist;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class DefaultTaskService {
+
+    private Map<Long, Task> database = new HashMap<>();
+    private Long TASK_ID_SEQUENCE = 0L;
+
+    public void createTask() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter task name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter task description: ");
+        String description = scanner.nextLine();
+
+        Task task = new Task();
+        task.setName(name);
+        task.setDescription(description);
+
+        task.setId(TASK_ID_SEQUENCE);
+
+        database.put(TASK_ID_SEQUENCE, task);
+
+        System.out.println("Task created, id:  " + task.getId());
+        TASK_ID_SEQUENCE++;
+    }
+
+    public void findTask() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter task id: ");
+        Long id = scanner.nextLong();
+        Task task = database.get(id);
+        System.out.println(task);
+    }
+}
---
+++ src/com/javaguru/todolist/ToDoListApplication.java
@@ -0,0 +1,4 @@
+package com.javaguru.todolist;
+
+class ToDoListApplication {
+}
